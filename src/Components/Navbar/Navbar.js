4fa614b69557580b411397f921a93bac9f6b6090
Tusar <tusarahmed141@gmail.com>
2022-08-03 15:29:43 +0600

    Make Navbar component

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <nav className="nav">
+        <a href="#home" className="nav__logo">
+          <img src="" alt="" className="nav__logo-img" />
+        </a>
+
+        <div className="nav__menu">
+          <ul className="nav__list">
+            <li className="nav__item">
+              <a href="#order" className="nav__link">
+                Order
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#order-review" className="nav__link">
+                Order Review
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#manage-inventory" className="nav__link">
+                Manage Inventory
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#login" className="nav__link">
+                Login
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
