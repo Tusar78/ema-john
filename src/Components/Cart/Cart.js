da3ca3a1ec98d849872dda4587e494b06c567904
Tusar <tusarahmed141@gmail.com>
2022-08-04 14:19:27 +0600

    Added cart button without any deisng

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { AiOutlineDelete, AiOutlineArrowRight } from 'react-icons/ai'
 
 const Cart = () => {
   return (
@@ -11,6 +12,16 @@ const Cart = () => {
         <p className='card__desc-single'>Tax: $37</p>
       </div>
       <h5 className="cart__grand-total">Grand Total: $2458</h5>
+      <div className="cart__buttons">
+        <button type="button" className="cart__btn cart__btn--danger">
+          <span>Clear Cart</span>
+          <AiOutlineDelete className='cart__btn-icon' />
+        </button>
+        <button type="button" className="cart__btn cart__btn--yellow">
+          <span>Clear Cart</span>
+          <AiOutlineArrowRight className='cart__btn-icon' />
+        </button>
+      </div>
     </div>
   );
 };
