da4dcd1c412614ed21ea9a920cdc72f6630d2c51
Tusar <tusarahmed141@gmail.com>
2022-09-24 15:16:06 +0600

    Authentication steps added

---
+++ src/_auth-steps.js
@@ -0,0 +1,6 @@
+/**
+ * 1, Create a new project in console.firebase.google.com.
+ * 2, yarn add firebase
+ * 3, Create Firebase.init.js and import getAuth to export auth
+ * 4, Firebase settings > Authentication > enable Email and password auth
+ */
