279438da2cbb8ab3558610df1faa6b4a4b972d04
Tusar <tusarahmed141@gmail.com>
2022-09-24 21:50:27 +0600

    Last update

--- src/_auth-steps.js
+++ src/_auth-steps.js
@@ -3,4 +3,6 @@
  * 2, yarn add firebase
  * 3, Create Firebase.init.js and import getAuth to export auth
  * 4, Firebase settings > Authentication > enable Email and password auth
+ * 5, Create login, Sign up component, setup route.
+ * 6, Attach form filed handler and form submit handler.
  */
