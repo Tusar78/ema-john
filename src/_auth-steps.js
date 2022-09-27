46d0a816be479c2986dc066bf74495ea254f4486
Tusar <tusarahmed141@gmail.com>
2022-09-27 19:43:37 +0600

    Auth steps update

---
+++ src/_auth-steps.js
@@ -0,0 +1,14 @@
+/**
+ * 1, Create a new project in console.firebase.google.com.
+ * 2, yarn add firebase.
+ * 3, Create Firebase.init.js and import getAuth to export auth.
+ * 4, Firebase settings > Authentication > enable Email and password auth.
+ * 5, Create login, Sign up component, setup route.
+ * 6, Attach form filed handler and form submit handler.
+ * 7, yarn add react-firebase-hooks.
+ * 8, useCreateUserWithEmailAndPassword from react-firebase-hooks.
+ * 9, If user is created redirect to the expected page.
+ * 10, useSignInWithEmailAndPassword for sign in.
+ * 11, Create RequireAuth component ==> check user exists also tract user location.
+ * 12, in route rap protected component by using RequireAuth component.
+ */
